c39fba5b04648759cd044772e99679496ce9102a
Mathew Seng <mathewseng@Mathews-MacBook-Pro.local>
2023-10-30 19:51:30 -0500

    Stats command

--- commands/stats.py
+++ commands/stats.py
@@ -7,15 +7,6 @@ class Stats(Command):
     def process(self): # TODO unlimited args for param support to query
         plants = DBUtil.read_plants()
 
-        # geni = set([p.genus for p in plants])
-        # plants_map = {}
-        #
-        # for p in plants:
-        #     if p['grade'] not in grouped_grades:
-        #         plants_map[p['grade']] = []
-        #
-        #     grouped_grades[item['grade']].append(item)
-
         print(f"Totals: {len(plants)}")
         print(f"Geni: {len(set([p.genus for p in plants]))}")
 
