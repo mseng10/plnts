26735d5ffbae8350aa8f055d82e46b8d5c70f8a7
Mathew Seng <mathewseng@Mathews-MacBook-Pro.local>
2023-11-05 16:39:17 -0600

    FUNC: Auto functionality

--- __main__.py
+++ __main__.py
@@ -1,14 +1,16 @@
 import sys
 from commands.create import Create
 from commands.stats import Stats
+from commands.need_water import NeedWaterCommand
+from datetime import datetime
 
 manual_commands = [Create("create"), Stats("stats")]
-auto_commands = []
+auto_commands = [NeedWaterCommand()]
 i = " "
 while True:
     i = sys.stdin.readline()
     key = str(i).strip()
-    if key == "quit":
+    if key == "quit" or key == "exit":
         break
     elif len(key) > 0:
         for command in manual_commands:
@@ -16,5 +18,6 @@ while True:
                 command.process()
     else:
         for command in auto_commands:
-            command.process()
+            if command.ready(datetime.now()):
+                command.process()
 print("GOODBYE")
--- commands/auto.py
+++ commands/auto.py
@@ -1,8 +1,16 @@
-class Auto_Command(Command):
-    def __int__(self, time, inc):
-        super().__init__("create")
-        self.time = time
-        self.next_time = time + inc
+from commands.command import Command
+from datetime import datetime, timedelta
 
-    def ready(self, curr_time):
-        return self.time >= curr_time
+class AutoCommand(Command):
+    def __init__(self, key: str, last_run: str, inc: int) -> None:  # duh
+        self.last_run: str = last_run
+        self.inc: int = inc
+        Command.__init__(self, key=key)
+
+    def ready(self, curr_time) -> bool:
+        return datetime.strptime(self.last_run, "%m-%d-%Y") + timedelta(days=self.inc) >= curr_time
+
+    def process(self) -> None:
+        super().process()
+        self.last_run: str = datetime.now().strftime('%m-%d-%Y')
+
