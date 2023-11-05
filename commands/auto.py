5d06199ca88c2d6451258a04b5114aa22cdd10fd
Mathew Seng <mathewseng@Mathews-MacBook-Pro.local>
2023-11-05 16:42:29 -0600

    STYLE: Black format

--- commands/auto.py
+++ commands/auto.py
@@ -1,6 +1,7 @@
 from commands.command import Command
 from datetime import datetime, timedelta
 
+
 class AutoCommand(Command):
     def __init__(self, key: str, last_run: str, inc: int) -> None:  # duh
         self.last_run: str = last_run
@@ -8,9 +9,11 @@ class AutoCommand(Command):
         Command.__init__(self, key=key)
 
     def ready(self, curr_time) -> bool:
-        return datetime.strptime(self.last_run, "%m-%d-%Y") + timedelta(days=self.inc) >= curr_time
+        return (
+            datetime.strptime(self.last_run, "%m-%d-%Y") + timedelta(days=self.inc)
+            >= curr_time
+        )
 
     def process(self) -> None:
         super().process()
-        self.last_run: str = datetime.now().strftime('%m-%d-%Y')
-
+        self.last_run: str = datetime.now().strftime("%m-%d-%Y")
--- commands/command.py
+++ commands/command.py
@@ -1,4 +1,3 @@
-
 class Command:
     """Abstract Command class."""
 
