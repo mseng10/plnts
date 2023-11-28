b9100c1d8616ef29468c90da923eb891fe9635e9
Mathew Seng <mathewseng@Mathews-MacBook-Pro.local>
2023-11-27 22:03:12 -0600

    FUNC: Stats cli

--- cli/stats.py
+++ cli/stats.py
@@ -1,23 +1,11 @@
-# from cli.process import Process
-#
-# from db import Session
-# from models.plant import Plant
-#
-#
-# class Stats(Process):
-#     """Calculate and display plant data set stats command."""
-#
-#     def __init__(self):
-#         Process.__init__(
-#             self,
-#             key="stats",
-#             description="Calculates and displays statistical analysis of the plants.",
-#         )
-#
-#     def process(self):  # TODO unlimited args for param support to query
-#         super().process()
-#         db = Session()
-#         plants: list[Plant] = db.query(Plant).all()
-#
-#         print(f"Totals: {len(plants)}")
-#         print(f"Geni: {len(set([p.genus for p in plants]))}")
+from models.plant import Plant
+from app import app, Session
+
+@app.cli.command("stats")
+def process(self):  # TODO unlimited args for param support to query
+    super().process()
+    db = Session()
+    plants: list[Plant] = db.query(Plant).all()
+
+    print(f"Totals: {len(plants)}")
+    print(f"Geni: {len(set([p.genus for p in plants]))}")
